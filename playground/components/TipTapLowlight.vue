cab094956ed329a80f3f9e6836366911514304a6
modbender <{ID}+{username}@users.noreply.github.com>
2024-08-09 22:47:31 +0530

    fix: lint

--- playground/components/TipTapLowlight.vue
+++ playground/components/TipTapLowlight.vue
@@ -2,30 +2,30 @@
   <div>
     <div v-if="editor">
       <button
-        @click="editor.chain().focus().toggleBold().run()"
         :disabled="!editor.can().chain().focus().toggleBold().run()"
         :class="{ 'is-active': editor.isActive('bold') }"
+        @click="editor.chain().focus().toggleBold().run()"
       >
         bold
       </button>
       <button
-        @click="editor.chain().focus().toggleItalic().run()"
         :disabled="!editor.can().chain().focus().toggleItalic().run()"
         :class="{ 'is-active': editor.isActive('italic') }"
+        @click="editor.chain().focus().toggleItalic().run()"
       >
         italic
       </button>
       <button
-        @click="editor.chain().focus().toggleStrike().run()"
         :disabled="!editor.can().chain().focus().toggleStrike().run()"
         :class="{ 'is-active': editor.isActive('strike') }"
+        @click="editor.chain().focus().toggleStrike().run()"
       >
         strike
       </button>
       <button
-        @click="editor.chain().focus().toggleCode().run()"
         :disabled="!editor.can().chain().focus().toggleCode().run()"
         :class="{ 'is-active': editor.isActive('code') }"
+        @click="editor.chain().focus().toggleCode().run()"
       >
         code
       </button>
@@ -36,68 +36,68 @@
         clear nodes
       </button>
       <button
-        @click="editor.chain().focus().setParagraph().run()"
         :class="{ 'is-active': editor.isActive('paragraph') }"
+        @click="editor.chain().focus().setParagraph().run()"
       >
         paragraph
       </button>
       <button
-        @click="editor.chain().focus().toggleHeading({ level: 1 }).run()"
         :class="{ 'is-active': editor.isActive('heading', { level: 1 }) }"
+        @click="editor.chain().focus().toggleHeading({ level: 1 }).run()"
       >
         h1
       </button>
       <button
-        @click="editor.chain().focus().toggleHeading({ level: 2 }).run()"
         :class="{ 'is-active': editor.isActive('heading', { level: 2 }) }"
+        @click="editor.chain().focus().toggleHeading({ level: 2 }).run()"
       >
         h2
       </button>
       <button
-        @click="editor.chain().focus().toggleHeading({ level: 3 }).run()"
         :class="{ 'is-active': editor.isActive('heading', { level: 3 }) }"
+        @click="editor.chain().focus().toggleHeading({ level: 3 }).run()"
       >
         h3
       </button>
       <button
-        @click="editor.chain().focus().toggleHeading({ level: 4 }).run()"
         :class="{ 'is-active': editor.isActive('heading', { level: 4 }) }"
+        @click="editor.chain().focus().toggleHeading({ level: 4 }).run()"
       >
         h4
       </button>
       <button
-        @click="editor.chain().focus().toggleHeading({ level: 5 }).run()"
         :class="{ 'is-active': editor.isActive('heading', { level: 5 }) }"
+        @click="editor.chain().focus().toggleHeading({ level: 5 }).run()"
       >
         h5
       </button>
       <button
-        @click="editor.chain().focus().toggleHeading({ level: 6 }).run()"
         :class="{ 'is-active': editor.isActive('heading', { level: 6 }) }"
+        @click="editor.chain().focus().toggleHeading({ level: 6 }).run()"
       >
         h6
       </button>
       <button
-        @click="editor.chain().focus().toggleBulletList().run()"
         :class="{ 'is-active': editor.isActive('bulletList') }"
+        @click="editor.chain().focus().toggleBulletList().run()"
       >
         bullet list
       </button>
       <button
-        @click="editor.chain().focus().toggleOrderedList().run()"
         :class="{ 'is-active': editor.isActive('orderedList') }"
+        @click="editor.chain().focus().toggleOrderedList().run()"
       >
         ordered list
       </button>
       <button
-        @click="editor.chain().focus().toggleCodeBlock().run()"
         :class="{ 'is-active': editor.isActive('codeBlock') }"
+        @click="editor.chain().focus().toggleCodeBlock().run()"
       >
         code block
       </button>
       <button
-        @click="editor.chain().focus().toggleBlockquote().run()"
         :class="{ 'is-active': editor.isActive('blockquote') }"
+        @click="editor.chain().focus().toggleBlockquote().run()"
       >
         blockquote
       </button>
@@ -108,14 +108,14 @@
         hard break
       </button>
       <button
-        @click="editor.chain().focus().undo().run()"
         :disabled="!editor.can().chain().focus().undo().run()"
+        @click="editor.chain().focus().undo().run()"
       >
         undo
       </button>
       <button
-        @click="editor.chain().focus().redo().run()"
         :disabled="!editor.can().chain().focus().redo().run()"
+        @click="editor.chain().focus().redo().run()"
       >
         redo
       </button>
