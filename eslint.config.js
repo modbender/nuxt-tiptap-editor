8d0a487d5cc92f973a5c7a5cdbb9a29d9b9212d3
modbender <{ID}+{username}@users.noreply.github.com>
2024-08-24 01:06:35 +0530

    fix: formatting errors for pages

--- eslint.config.js
+++ eslint.config.js
@@ -2,4 +2,7 @@ import { createConfigForNuxt } from '@nuxt/eslint-config/flat';
 
 export default createConfigForNuxt({
   ignores: ['dist', 'node_modules'],
+  rules: {
+    'vue/multi-word-component-names': 0,
+  },
 });
