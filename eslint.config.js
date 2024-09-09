1f4bdaa469d594967f26c24b8c9db987761bc7ed
modbender <{ID}+{username}@users.noreply.github.com>
2024-09-09 23:38:08 +0530

    chore: lint fix, upgrade packages

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,7 @@
 // @ts-check
-import { createConfigForNuxt } from '@nuxt/eslint-config/flat'
+import { createConfigForNuxt } from '@nuxt/eslint-config/flat';
+
+const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
 
 // Run `npx @eslint/config-inspector` to inspect the resolved config interactively
 export default createConfigForNuxt({
@@ -10,11 +12,9 @@ export default createConfigForNuxt({
     stylistic: true,
   },
   dirs: {
-    src: [
-      './playground',
-    ],
+    src: ['./playground'],
   },
-})
-  .append(
-    // your custom flat config here...
-  )
+}).append(
+  // your custom flat config here...
+  eslintPluginPrettierRecommended,
+);
