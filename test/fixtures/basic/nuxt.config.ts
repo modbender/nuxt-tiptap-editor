f46e1c4a34a96ad8b54442a11aa5aca85716005c
modbender <rameshmamathayashas@gmail.com>
2025-04-29 20:30:28 +0530

    fix: test

--- test/fixtures/basic/nuxt.config.ts
+++ test/fixtures/basic/nuxt.config.ts
@@ -2,4 +2,13 @@ import MyModule from '../../../src/module'
 
 export default defineNuxtConfig({
   modules: [MyModule],
+  vite: {
+    build: {
+      rollupOptions: {
+        output: {
+          assetFileNames: 'assets/[name].[hash][extname]',
+        },
+      },
+    },
+  },
 })
